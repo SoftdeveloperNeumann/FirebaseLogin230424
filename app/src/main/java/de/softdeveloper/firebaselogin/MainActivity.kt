7ed03a52807e890162a4e5dc975533424e0b6e10
Frank <f.neumann.berlin@googlemail.com>
2023-04-24 11:15:08 +0200

    grundgerüßt und Layout fertig

--- app/src/main/java/de/softdeveloper/firebaselogin/MainActivity.kt
+++ app/src/main/java/de/softdeveloper/firebaselogin/MainActivity.kt
@@ -1,11 +1,34 @@
 package de.softdeveloper.firebaselogin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import de.softdeveloper.firebaselogin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private var user: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+        user = auth.currentUser
+
+        if(user == null){
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }else{
+            Toast.makeText(this, "Du bist angemeldet", Toast.LENGTH_SHORT).show()
+        }
     }
 }
---
+++ app/src/main/java/de/softdeveloper/firebaselogin/RegisterActivity.kt
@@ -0,0 +1,10 @@
+package de.softdeveloper.firebaselogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
